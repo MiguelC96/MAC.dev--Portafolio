a19ea034945a1e3a74b22379ffc346e2457ea2f8
MiguelC96 <129960886+MiguelC96@users.noreply.github.com>
2024-08-16 14:30:23 -0300

    modified:   about.html 	modified:   index.html 	new file:   main.js 	modified:   styles.css

---
+++ main.js
@@ -0,0 +1,4 @@
+document.querySelector('.menu-button').addEventListener('click', function() {
+    const navMenu = document.querySelector('.navigation-items');
+    navMenu.classList.toggle('open');
+});

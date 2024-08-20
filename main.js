355ca4d47ee1074cf1e2e5b0e5bc2c726021bfce
MiguelC96 <129960886+MiguelC96@users.noreply.github.com>
2024-08-20 01:29:32 -0300

    Changes to be committed: 	modified:   about.html 	new file:   desarrollo.html 	renamed:    project-1.html -> "dise\303\261oweb.html" 	modified:   encriptador.html 	new file:   images/carru_encrip1.png 	new file:   images/certificados/javascript.png 	new file:   images/certificados/scrum.png 	modified:   index.html 	modified:   main.js 	deleted:    project-2.html 	modified:   styles.css

--- main.js
+++ main.js
@@ -2,3 +2,23 @@ document.querySelector('.menu-button').addEventListener('click', function() {
     const navMenu = document.querySelector('.navigation-items');
     navMenu.classList.toggle('open');
 });
+
+function copyCode() {
+    // Seleccionar el contenido del código
+    const codeBlock = document.getElementById('code-block');
+    const range = document.createRange();
+    range.selectNode(codeBlock);
+    window.getSelection().removeAllRanges();
+    window.getSelection().addRange(range);
+
+    try {
+        // Intentar copiar al portapapeles
+        document.execCommand('copy');
+        alert('Código copiado al portapapeles!');
+    } catch (err) {
+        alert('No se pudo copiar el código.');
+    }
+
+    // Limpiar la selección
+    window.getSelection().removeAllRanges();
+}
